c74f6af155ca8a1b1b8cdd2b562e106019c98efb
sla-ppy <sla-ppy-pm@protonmail.com>
2025-03-13 20:12:54 +0100

    fix first errr_list usage

--- src/main.c
+++ src/main.c
@@ -6,7 +6,8 @@
 
 // returns the correct, length-guaranteed errr message
 char* getErrrMsg(int errr_code) {
-    char* errr_list[7] = {
+    char* errr_list[8] = {
+        "[ERRR] Errr list contains string literal, where length is incorrect!\n",
         "[ERRR] Usage: ./main <message>\n",
         "[ERRR] Message too long. Maximum size allowed: 255\n",
         "[ERRR] Message must start with character '*'. Format: [*CCCC:par,par##]\n",
@@ -20,7 +21,7 @@ char* getErrrMsg(int errr_code) {
     int errr_list_size = sizeof(errr_list) / sizeof(*errr_list);
     for (int i = 0; i < errr_list_size; i++) {
         if (strlen(errr_list[i]) > 126) { // 126, +1 for null terminator
-            printf("[ERRR] Errr list contains string literal, where length is incorrect!\n");
+            printf("%s", errr_list[0]);
             exit(1);
         }
     }
@@ -32,18 +33,18 @@ char* getErrrMsg(int errr_code) {
 int paramCheck(int argc, char** argv, char** message) {
     // basic usage checks
     if (argc < 2) {
-        printf("%s", getErrrMsg(0));
+        printf("%s", getErrrMsg(1));
         return 1;
     }
     if (argv[1] == NULL) {
-        printf("%s", getErrrMsg(0));
+        printf("%s", getErrrMsg(1));
         return 1;
     }
 
     // messages can be only 255 long +1 for null terminator
     const int len = strlen(argv[1]);
     if (len > 255) {
-        printf("%s", getErrrMsg(1));
+        printf("%s", getErrrMsg(2));
         return 1;
     } else {
         *message = strdup(argv[1]);
@@ -52,21 +53,21 @@ int paramCheck(int argc, char** argv, char** message) {
     // messages starts with *
     // NOTE: as operator precedence thinks i wanna do *(message[0]) => instead of (*message)[0], which is how we access the chars properly in this case
     if ((*message)[0] != '*') {
-        printf("%s", getErrrMsg(2));
+        printf("%s", getErrrMsg(3));
         return 1;
     }
 
     // CCCC can only be uppercase letters, guarantee its exactly 4 chars long too
     for (int i = 1; i < 5; i++) {
         if (isupper((*message)[i]) == 0 || isdigit((*message)[i]) != 0) {
-            printf("%s", getErrrMsg(3));
+            printf("%s", getErrrMsg(4));
             return 1;
         }
     }
 
     // : after CCCC
     if ((*message)[5] != ':') {
-        printf("%s", getErrrMsg(4));
+        printf("%s", getErrrMsg(5));
         return 1;
     }
 
@@ -75,7 +76,7 @@ int paramCheck(int argc, char** argv, char** message) {
     // 256 -> \0
     // last two chars is #
     if ((*message)[strlen(*message) - 1] != '#' || (*message)[strlen(*message) - 2] != '#') {
-        printf("%s", getErrrMsg(5));
+        printf("%s", getErrrMsg(6));
         return 1;
     }
 
@@ -108,7 +109,7 @@ int main(int argc, char** argv) {
         }
 
         if (i == command_list_size - 1) {
-            printf("%s", getErrrMsg(6));
+            printf("%s", getErrrMsg(7));
             return 1;
         }
     }
